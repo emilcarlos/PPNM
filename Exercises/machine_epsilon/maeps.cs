e208a71cef06232b40258370ce167da4417fe48e
emilcarlos <emilclausen6@gmail.com>
2023-02-10 13:44:12 +0100

    uploaded files for machine_epsilon task

---
+++ Exercises/machine_epsilon/maeps.cs
@@ -0,0 +1,9 @@
+using System;
+
+class maeps{
+	static void Main(){
+	int i = 1;
+	while(i+1>i) {i++;}
+	Console.WriteLine("my max int = {0}", i);
+	} 
+}
---
+++ Exercises/machine_epsilon/main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class prime {
+	static void Main(){
+		
+
+
+	
+	}
+}
