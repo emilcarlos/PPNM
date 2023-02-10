e2569b89975c8c637270f7bad27044a7730ace5b
emilcarlos <emilclausen6@gmail.com>
2023-02-10 15:11:45 +0100

    updates

--- Exercises/machine_epsilon/maeps.cs
+++ Exercises/machine_epsilon/maeps.cs
@@ -11,5 +11,18 @@ class maeps{
 		Console.WriteLine("my min int = {0}", j);
 
 		Console.WriteLine("MinValue = {0}", int.MinValue);
+
+		double x = 1;
+		while(1+x != 1){x/=2;}
+		x*=2;
+		float y = 1F;
+		while(1F+y != 1F){y/=2;}
+		y*=2;
+		double check_double = Math.Pow(2, -52);
+		double check_float = Math.Pow(2, -23);
+		Console.WriteLine("machine epsilon for double = " + x);
+		Console.WriteLine("machine epsilon for float " + y);
+		Console.WriteLine("check for double = " + check_double);
+		Console.WriteLine("check for float = ", + check_float);
 	} 
 }
