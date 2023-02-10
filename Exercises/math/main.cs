c295705dfdc7b16c3c5eac70c5d3949feee35962
emilcarlos <emilclausen6@gmail.com>
2023-02-10 10:01:11 +0100

    updates

--- Exercises/math/main.cs
+++ Exercises/math/main.cs
@@ -5,8 +5,16 @@ class first {
 	static void Main()
 	{
 		double square = Math.Sqrt(2);
-		Console.WriteLine("Math.E = " + square);
+		Console.WriteLine("sqrt(2) = " + square);
+		
+		double power = Math.Pow(2, 0.2);
+		Console.WriteLine("2 to the power of 1/5 = " + power);
+		
+		double e_pi = Math.Pow(Math.E, Math.PI);
+		Console.WriteLine("e to the power of pi = " + e_pi);
 
+		double pi_e = Math.Pow(Math.PI, Math.E);
+		Console.WriteLine("pi to the power of e = " + pi_e);
 
 	}
 }
