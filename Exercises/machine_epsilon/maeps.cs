df0f39d9dd901c0d42b4d9a198267a49b87f4dfe
emilcarlos <emilclausen6@gmail.com>
2023-02-10 14:34:58 +0100

    updates

--- Exercises/machine_epsilon/maeps.cs
+++ Exercises/machine_epsilon/maeps.cs
@@ -2,8 +2,12 @@ using System;
 
 class maeps{
 	static void Main(){
-	int i = 1;
-	while(i+1>i) {i++;}
-	Console.WriteLine("my max int = {0}", i);
+		int i = 1;
+		while(i+1>i) {i++;}
+		Console.WriteLine("my max int = {0}", i);
+
+		int j = -1;
+		while(j-1<j) {j--;}
+		Console.WriteLine("my min int = {0}", j);
 	} 
 }
