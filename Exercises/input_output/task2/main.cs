08c232e99b41b8169d9db5ae54b127f8eefd9bfc
emilcarlos <emilclausen6@gmail.com>
2023-02-28 15:16:05 +0100

    updates

--- Exercises/input_output/task2/main.cs
+++ Exercises/input_output/task2/main.cs
@@ -2,15 +2,17 @@ using System;
 using static System.Console;
 using static System.Math;
 
-public static class inout2{
+public static class main{
 	public static void Main(string[] args){
-	char[] split_delimiters = {' ', '\t','n'};
-		foreach(var arg in args){
-		var numbers = arg.Split(split_delimiters);
+		char[] split_delimiters = {' ','\t','\n'};
+		var split_options = StringSplitOptions.RemoveEmptyEntries;
+		for( string line = ReadLine(); line != null; line = ReadLine() ){
+			var numbers = line.Split(split_delimiters,split_options);
 			foreach(var number in numbers){
-			double x = double.Parse(number);
-			WriteLine($"{x} {Sin(x)} {Cos(x)}");
-			}
+				double x = double.Parse(number);
+				WriteLine($"{x} {Sin(x)} {Cos(x)}");
+				}
 		}
 	}
 }
+
