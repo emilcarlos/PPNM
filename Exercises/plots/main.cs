9d765604d454015e66dd9c629e812579839dd0e0
emilcarlos <emilclausen6@gmail.com>
2023-03-22 13:12:17 +0100

    updates

--- Exercises/plots/main.cs
+++ Exercises/plots/main.cs
@@ -1,8 +1,12 @@
 using static System.Console;
 class main{
-static void Main(){
+	public static void Main(string[] args){
+	foreach(var arg in args){
+	if(arg=="gamma"){
 	for(double x=-5+1.0/128;x<=5;x+=1.0/64){
 		WriteLine($"{x} {sfuns.gamma(x)}");
 	}
-}
+	}
+	}
+	}
 }
