91f248cc3d49a105a1e65fd29373fc46455c2df0
emilcarlos <emilclausen6@gmail.com>
2023-06-29 04:06:38 +0200

    final commit

--- Homeworks/main.cs
+++ Homeworks/main.cs
@@ -5,16 +5,16 @@ using static System.Math;
 class main{
 	public static class funcs{
 		public static (vector,vector) rkstep12(
-		Func<double,vector,vector> f /* the f from dy/dx=f(x,y) */
-		double x,                    /* the current value of the variable */
-		vector y,                    /* the current value y(x) of the sought function */
-		double h                     /* the step to be taken */
+		Func<double,vector,vector> f
+		double x,                    
+		vector y,                    
+		double h                     
 		)
 		{
-		vector k0 = f(x,y);              /* embedded lower order formula (Euler) */
-		vector k1 = f(x+h/2,y+k0*(h/2)); /* higher order formula (midpoint) */
-		vector yh = y+k1*h;              /* y(x+h) estimate */
-		vector er = (k1-k0)*h;           /* error estimate */
+		vector k0 = f(x,y);              
+		vector k1 = f(x+h/2,y+k0*(h/2)); 
+		vector yh = y+k1*h;              
+		vector er = (k1-k0)*h;           
 		return (yh,er);
 		}
 	}
